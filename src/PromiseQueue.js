311e0d5360b9b9cc57a3cb938418e1fec5df7bc4
Mike Hollingshaus <mike.haus.dev@gmail.com>
2023-03-29 06:47:27 -0600

    build basic concurrent promise queue

--- src/PromiseQueue.js
+++ src/PromiseQueue.js
@@ -5,13 +5,39 @@ const DND_URL_1 = 'classes/barbarian';
 const DND_URL_2 = 'damage-types/necrotic';
 const DND_URL_3 = 'magic-items/adamantine-armor';
 
-const barbarianData = axios.get(`${DND_BASE_URL}${DND_URL_1}`).then(resp => resp?.data);
-const necroticData = axios.get(`${DND_BASE_URL}${DND_URL_2}`).then(resp => resp?.data);
-const armorData = axios.get(`${DND_BASE_URL}${DND_URL_3}`).then(resp => resp?.data);
+const barbarianData = axios.get(`${DND_BASE_URL}${DND_URL_1}`).then(resp => console.log(resp?.data));
+const necroticData = axios.get(`${DND_BASE_URL}${DND_URL_2}`).then(resp => console.log(resp?.data));
+const armorData = axios.get(`${DND_BASE_URL}${DND_URL_3}`).then(resp => console.log(resp?.data));
 
 const dndData = [barbarianData, necroticData, armorData];
 
-const getDndData = () => Promise.all(dndData).then(values => console.log(values));
+function PromiseQueue(tasks = [], concurrentCount = 1) {
+  this.total = tasks.length;
+  this.todo = tasks;
+  this.running = [];
+  this.complete = [];
+  this.count = concurrentCount;
+}
+
+PromiseQueue.prototype.runNext = function() {
+  return ((this.running.length < this.count) && this.todo.length)
+}
+
+PromiseQueue.prototype.run = function() {
+  while (this.runNext()) {
+    const promise = this.todo.shift();
+    promise.then(() => {
+      this.complete.push(this.running.shift());
+      this.run();
+    });
+    this.running.push(promise);
+  }
+}
+
+const getDndData = () => {
+  const promiseQueue = new PromiseQueue(dndData, 3);
+  promiseQueue.run();
+}
 
 export { getDndData };
 
